a1b8ebcc91d3f81bcae53e98b7797105794b38c7
Anthony Picciano <anthony.picciano@gmail.com>
2014-05-20 08:24:18 -0600

    Initial Commit

---
+++ iSwizzle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iSwizzle
+//
+//  Created by Anthony Picciano on 5/20/14.
+//  Copyright (c) 2014 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iSwizzle/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  iSwizzle
+//
+//  Created by Anthony Picciano on 5/20/14.
+//  Copyright (c) 2014 Anthony Picciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
